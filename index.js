2264fd27a4cc05eeafe476e383b76b5467755f0d
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-04-29 11:22:17 -0400

    POST/GET Requests for new article

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import {cohereClassify, cohereSummary} from "./cohereSummary.js";
 import {writeMongo} from "./mongoDatabase.js";
 import cohere from "cohere-ai";
 
+// const { json } = require('express');
+import {json} from "express";
+// var express  = require('express');
+import express from "express";
+var app = express();
+var port = 3000;
+
 let puppeteerSelector = "";
 let websiteText = "";
 let summary = "";
@@ -15,38 +22,65 @@ let candidate = "";
 // const testWebsite = "https://www.cbc.ca/news/canada/toronto/olivia-chow-toronto-mayor-byelection-1.6812750";
 // const testWebsite = "https://www.cbc.ca/news/canada/toronto/city-council-docking-pay-josh-matlow-1.6798416";
 
-if (testWebsite.includes("cbc")) {
-    puppeteerSelector = ".story";
-    websiteText = await scrapeWebsite(testWebsite, puppeteerSelector);
-    websiteText = websiteText[0];
-    network = "CBC";
-}
-else if (testWebsite.includes("ctv")) {
-    puppeteerSelector = ".c-text";
-    websiteText = await scrapeWebsite(testWebsite, puppeteerSelector);
-    websiteText = websiteText[0].replace(/\\n.*\\t/, '').replace(/\s+/g,' ').trim();
-    network = "CTV";
-}
-else if (testWebsite.includes("cp24")) {
-    puppeteerSelector = ".articleBody";
-    websiteText = await scrapeWebsite(testWebsite, puppeteerSelector);
-    websiteText = websiteText[0].replace(/\\n.*\\t/, '').replace(/\s+/g,' ').trim();
-    network = "CP24";
-}
-else if (testWebsite.includes("thestar")) {
-    puppeteerSelector = ".text-block-container";
-    websiteText = await scrapeWebsite(testWebsite, puppeteerSelector);
-    websiteText = websiteText.join(" ").replace(/\s+/g,' ').trim();
-    network = "Toronto Star";
-}
-else {
-    console.log("Unsupported website");
-}
+app.post('/post', async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log("New express client");
+    console.log("Received: ");
+    console.log(JSON.parse(req.query['data']));
+    var requestInfo = JSON.parse(req.query['data']);
 
-// console.log(websiteText);
-summary = await cohereSummary(websiteText);
-console.log(summary);
-candidate = await cohereClassify(websiteText);
-// console.log(candidate);
+    if (requestInfo['action'].includes('newArticle')) {
+        console.log(requestInfo['link']);
+        let message = await newArticle(requestInfo['link']);
+        var jsontext = JSON.stringify({
+            'action': 'newArticle',
+            'message': message
+        });
+        res.send(jsontext);
+    } else if (requestInfo['action'].includes('guess')) {
+        var jsontext = JSON.stringify({
+            'action': 'guess',
+        });
+        res.send(jsontext);
+    }
+}).listen(3000);
+console.log("Server is running! (listening on http://localhost:" + port + ")");
 
-// await writeMongo(testWebsite, network, summary, candidate, 0.9, 0.7);
+async function newArticle(testWebsite) {
+    if (testWebsite.includes("cbc")) {
+        puppeteerSelector = ".story";
+        websiteText = await scrapeWebsite(testWebsite, puppeteerSelector);
+        websiteText = websiteText[0];
+        network = "CBC";
+    }
+    else if (testWebsite.includes("ctv")) {
+        puppeteerSelector = ".c-text";
+        websiteText = await scrapeWebsite(testWebsite, puppeteerSelector);
+        websiteText = websiteText[0].replace(/\\n.*\\t/, '').replace(/\s+/g,' ').trim();
+        network = "CTV";
+    }
+    else if (testWebsite.includes("cp24")) {
+        puppeteerSelector = ".articleBody";
+        websiteText = await scrapeWebsite(testWebsite, puppeteerSelector);
+        websiteText = websiteText[0].replace(/\\n.*\\t/, '').replace(/\s+/g,' ').trim();
+        network = "CP24";
+    }
+    else if (testWebsite.includes("thestar")) {
+        puppeteerSelector = ".text-block-container";
+        websiteText = await scrapeWebsite(testWebsite, puppeteerSelector);
+        websiteText = websiteText.join(" ").replace(/\s+/g,' ').trim();
+        network = "Toronto Star";
+    }
+    else {
+        console.log("Unsupported website");
+        return "Unsupported website";
+    }
+
+    // console.log(websiteText);
+    summary = await cohereSummary(websiteText);
+    console.log(summary);
+    candidate = await cohereClassify(websiteText);
+    // console.log(candidate);
+    // await writeMongo(testWebsite, network, summary, candidate, 0.9, 0.7);
+    return "Wrote to database";
+}
---
+++ web_client.js
@@ -0,0 +1,27 @@
+let url = "http://localhost:3000/post";
+
+function newArticle() {
+    $.post(url+'?data='+JSON.stringify({
+        'action': 'newArticle',
+        'link': document.getElementById("articleLink").value
+    }), response)
+    console.log("Sent article!");
+}
+
+function response(data, status) {
+    var response = JSON.parse(data);
+    console.log(data);
+    // if (response['action'] == 'generateNumber') {
+    //     userName = response['nameId'];
+    //     console.log(userName);
+    // }
+    // else if (response['action'] == 'guess') {
+    //     if (response['result']) {
+    //         correctGuess();
+    //     }
+    //     else {
+    //         wrongGuess(response['difference'], response['number']);
+    //     }
+    // }
+}
+
