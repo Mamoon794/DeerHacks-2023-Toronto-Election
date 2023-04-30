0280f534be140b5849aee366d1a7136df1ceef83
Mamoon Akhtar <primus@Optimus-Primes-Computer.local>
2023-04-30 01:16:20 -0400

    hopefully done

--- frontEnd/main.py
+++ frontEnd/main.py
@@ -9,40 +9,57 @@ url = "http://localhost:3000/post?data="
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.INFO)
 
-categories = ['protransit', 'prohousing', 'anticrime']
-people = [{
-    'name': 'Mark saunders',
-    'last': 'saunders',
-    },
-    {
-        'name': 'Olivia Chow',
-        'last': 'chow',
-
-    },
+categories = {'Transit': 'protransit', 'Housing': 'prohousing', 'Crime': 'anticrime'}
+people = [
     {
         'name': 'Ana Bailao',
         'last': 'bailao',
 
     },
     {
-        'name':'Josh Matlow',
-        'last': 'matlow',
-    },
-    {
         'name': 'Brad Bradford',
         'last': 'bradford',
     },
     {
+        'name': 'Josh Matlow',
+        'last': 'matlow',
+    },
+    {
+        'name': 'Mark Saunders',
+        'last': 'saunders',
+    },
+    {
         'name': 'Mitzie Hunter',
         'last': 'hunter',
+    },
+    {
+        'name': 'Olivia Chow',
+        'last': 'chow',
+
     }]
 
 
 count  = 0
+stuff = []
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    global stuff
     url = "http://localhost:3000/post?data="
     if request.method == 'POST':
+        if "submit" in request.form:
+            getUrl = request.form.get("give url")
+            if "youtube" in getUrl.lower():
+                data = {"url": getUrl}
+                data = json.dumps(data)
+                response = requests.post("http://localhost:5001/", json=data)
+                response_data = response.json()
+                if response_data == 200:
+                    print("okay")
+
+
+            return render_template('home.html', options=stuff[0], categories=stuff[1], politician=stuff[2],
+                                   category=stuff[3], info=stuff[4])
+
         if "politician" in request.form and "categories" in request.form:
             temp_pep = people.copy()
             temp_cat = categories.copy()
@@ -54,46 +71,40 @@ def home():
                     theperson = pep
                     temp_pep.remove(pep)
 
-            for cat in categories:
-                if cat == category:
-                    temp_cat.remove(cat)
+            theCategory = categories[category]
+            temp_cat.pop(category)
 
-            # return theperson[f'{category}']
-            data = {"action": "retrieveArticle", "candidate": theperson["last"], "sentiment": category}
+            data = {"action": "retrieveArticle", "candidate": theperson["last"], "sentiment": theCategory}
             json_data = json.dumps(data)
             url += json_data
             response = requests.post(url, json=json_data)
             response_data = response.json()
-            print(response_data)
 
             make_info = []
 
             for respon in response_data['message']['documents']:
                 if respon['sentiment'] == 'protransit':
-                    make_info.append([respon['summary'], round(float(respon['transitSentiment']), 2),
+                    make_info.append([respon['summary'][:200], round(float(respon['transitSentiment']), 2),
                                       respon['link'], respon['network']])
 
                 if respon['sentiment'] == 'prohousing':
-                    make_info.append([respon['summary'], round(float(respon['housingSentiment']), 2),
+                    make_info.append([respon['summary'][:200], round(float(respon['housingSentiment']), 2),
                                       respon['link'], respon['network']])
 
                 if respon['sentiment'] == 'anticrime':
-                    make_info.append([respon['summary'], round(float(respon['crimeSentiment']), 2),
+                    make_info.append([respon['summary'][:200], round(float(respon['crimeSentiment']), 2),
                                       respon['link'], respon['network']])
 
-
-
-
+            stuff = [temp_pep, temp_cat, theperson, category, make_info]
             return render_template('home.html', options=temp_pep, categories=temp_cat, politician=theperson,
                                    category=category, info=make_info)
 
         if "categories" in request.form:
             temp_cat = categories.copy()
             category = request.form.get('categories')
-            for cat in categories:
-                if cat == category:
-                    temp_cat.remove(cat)
-                    return render_template('home.html', options=people, categories=temp_cat, category=category)
+            theCategory = categories[category]
+            temp_cat.pop(category)
+            return render_template('home.html', options=people, categories=temp_cat, category=theCategory)
 
         if "politician" in request.form:
             temp_pep = people.copy()
